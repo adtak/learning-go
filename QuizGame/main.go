e59206d488908253d43a6054346010972f6afb21
adtak <adtak62@gmail.com>
2023-09-04 21:34:24 +0900

    go fmt

--- QuizGame/main.go
+++ QuizGame/main.go
@@ -9,20 +9,20 @@ import (
 
 func main() {
 	file, err := os.Open("problems.csv")
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer file.Close()
-  
-  r := csv.NewReader(file)
-  rows, err := r.ReadAll()
-  if err != nil {
-    log.Fatal(err)
-  }
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	r := csv.NewReader(file)
+	rows, err := r.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	var score, amount int32
-  for i, v := range rows {
-    question, answer := v[0], v[1]
+	for i, v := range rows {
+		question, answer := v[0], v[1]
 		fmt.Printf("Question #%v: %v\n", i+1, question)
 		var input string
 		fmt.Scan(&input)
@@ -31,6 +31,6 @@ func main() {
 			score += 1
 		}
 		amount += 1
-  }
+	}
 	fmt.Printf("You scored %v out of %v\n", score, amount)
 }
